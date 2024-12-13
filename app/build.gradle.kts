d3b27e812d129f78167a39f1863b4cf3350aa528
F3rny <20233tn099utez.edu.mx>
2024-12-13 00:09:05 -0600

    Cambios de la configuracion del gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.firebase.database)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
